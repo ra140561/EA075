5de97a6dc80d99eaa9b92442c3eaad5407d29395
Daniel Cunha <daniel.cunha.x23@gmail.com>
2015-11-24 21:38:39 -0200

    Updated VHDL files

--- Etapa4/VHDL/CRIPT.vhd
+++ Etapa4/VHDL/CRIPT.vhd
@@ -5,12 +5,16 @@ USE ieee.std_logic_1164.all;
 
 ENTITY CRIPT IS
 PORT (	IN_CHAR, CRIPT_CHAR:	in std_logic_vector(7 downto 0);
+	ENABLE:			in std_logic;
 	OUT_CHAR:		out std_logic_vector(7 downto 0));
 END CRIPT;
 
 ARCHITECTURE CRIPTING OF CRIPT IS
 
+SIGNAL ENABLE_8 : STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');
+
 BEGIN
-	OUT_CHAR <= IN_CHAR XOR CRIPT_CHAR;
+	ENABLE_8 <= (OTHERS => ENABLE);
+	OUT_CHAR <= (IN_CHAR XOR CRIPT_CHAR) AND ENABLE_8;
 
 END CRIPTING;
